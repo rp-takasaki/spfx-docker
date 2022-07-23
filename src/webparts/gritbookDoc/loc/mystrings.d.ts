3d32c14ef052fbf25fb6be8605ec40a0534ea632
takasaki <tomoyuki.takasaki@regrit-p.com>
2022-07-23 14:35:09 +0000

    initial commit

---
+++ src/webparts/gritbookDoc/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IGritbookDocWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'GritbookDocWebPartStrings' {
+  const strings: IGritbookDocWebPartStrings;
+  export = strings;
+}
